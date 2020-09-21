0e55336789eb95a44dc408f908a9445053a45762
Arjun <36031200+A-r-jun@users.noreply.github.com>
2020-09-21 21:20:35 +0530

    Add files via upload

---
+++ Array_minmax.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Array_minmax {
+
+	public static void main(String[] args) 
+{  Scanner s= new Scanner(System.in);
+		int n=0;
+		
+		
+		int[] array = new int [n];
+		
+		
+}
+}
